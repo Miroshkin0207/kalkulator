1ccba36643480d0d07e7a336636720bdd61c2912
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-03-15 21:18:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function vichislenie() {
+let first = document.getElementById("first").value
+let second = document.getElementById("second").value
+let result = first + second
+document.getElementById("ravno").innerHTML = "="
+document.getElementById("result").innerHTML = result
+}
