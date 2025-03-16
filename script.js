cf22014beb0e0302d9a89f5c5922bf7dddef8174
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-03-16 10:08:37 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function vichislenie() {
-let first = document.getElementById("first").value
-let second = document.getElementById("second").value
-let result = first + second
-document.getElementById("ravno").innerHTML = "="
-document.getElementById("result").innerHTML = result
-}
