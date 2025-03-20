0e464aaec5d524550992f7aea2b66ff7b6af96db
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-03-20 21:06:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function vichislenie() {
+    let knopka = document.getElementById("vichislit")
+    let first = Number(document.getElementById("first").value)
+    let second = Number(document.getElementById("second").value)
+    let result = Number(first + second)
+    let ravno = document.getElementById("ravno")
+    let resultat = document.getElementById("result")
+    if (knopka.innerHTML === "Вычислить") {
+      ravno.innerHTML = "Результат:"
+      resultat.innerHTML = result
+      knopka.innerHTML = "Сбросить"
+    }
+    else if (knopka.innerHTML === "Сбросить") {
+      ravno.innerHTML = ""
+      resultat.innerHTML = ""
+      document.getElementById("first").innerHTML=""
+      document.getElementById("second").innerHTML=""
+      knopka.innerHTML = "Вычислить"    
+    }
+  }
