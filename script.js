78d7cc7f8b75166e601d1ffb21393e7b1c0316f7
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-03-20 20:13:44 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function vichislenie() {
-let first = Number(document.getElementById("first").value)
-let second = Number(document.getElementById("second").value)
-let result = Number(first + second)
-document.getElementById("ravno").innerHTML = "Результат:"
-document.getElementById("result").innerHTML = result
-}
